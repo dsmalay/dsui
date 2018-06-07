bafb9845e4960f4fc9ac79ebd270c89fe2dcfd2d
mcgeeluo <mcgeeluo01@outlook.com>
2018-06-07 18:47:59 +0800

    初始版本测试

--- src/components/DsLogin.vue
+++ src/components/DsLogin.vue
@@ -1,26 +1,34 @@
 <template>
 
     <div>
-       <div id="login" v-if="type == 1">
-            <h2 class="login-title">center</h2>
-            <div class="login-box">
-                <el-form  label-width="60px" class="demo-ruleForm">
-                <el-form-item  >
-                    <el-input prefix-icon="icon-account1" type="text"  placeholder="请填写用户名"></el-input>
-                </el-form-item>
-                <el-form-item porp="password">
-                    <el-input type="password" prefix-icon="icon-Password" placeholder="请填写密码"></el-input>
-                </el-form-item>
-                <el-form-item>
-                    <el-button type="primary" class="button" >立即登录</el-button>
-                </el-form-item>
-                <el-form-item>
-                    <el-checkbox>我已阅读并同意</el-checkbox>
-                    <el-button type="text">用户协议</el-button>
-                </el-form-item>
-                <el-form-item>
-                    <el-button type="text" class="button">试玩</el-button>
-                </el-form-item>
+       <div id="login" >
+            <div class="login-box" id="box">
+                <el-form  label-width="60px" class="">
+                    <el-form-item  >
+                        <el-input prefix-icon="icon-account1" type="text"  placeholder="请填写用户名"></el-input>
+                    </el-form-item>
+                    <el-form-item >
+                        <el-input type="password" prefix-icon="icon-Password" placeholder="请填写密码"></el-input>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-button type="primary" class="button" >立即登录</el-button>
+                    </el-form-item>
+                    <el-form-item style="display:none">
+                        <el-checkbox>我已阅读并同意</el-checkbox>
+                        <el-button type="text">用户协议</el-button>
+                    </el-form-item>
+                    <el-form-item style="display:none">
+                            <span>域名验证</span>
+                            <span>联系客服</span>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-radio style="display:none">记住密码</el-radio>
+                        <span style="display:inlne-block">
+                            <el-checkbox>同意</el-checkbox>
+                            <span>协议与条款</span>
+                        </span>
+                        <el-button type="text" >试玩</el-button>
+                    </el-form-item>
                 </el-form>
                 <el-tabs>
                     <el-button type="text">线路检测</el-button>
@@ -37,11 +45,11 @@
                     <el-button type="text">IE</el-button>
                 </el-tabs>
             </div>
-        </div>
+       </div>
 
         <!-- 登陆框在右边的 -->
-        <div id="login"  v-if="type == 2">
-            <div class="login-title" id="right">
+        <!-- <div id="login"  v-if="type == 2">
+            <div>
                 <h2>right</h2>
                 <p></p>
             </div>
@@ -66,51 +74,57 @@
                 <p >建议使用1366*768以上分辨率，并使用谷歌浏览器，火狐浏览器，IE9以上浏览器浏览本站</p>
                 
             </div>
-        </div>
+        </div> -->
     </div>
 </template>
 
 <script>
-    export default {
-        props:['type'],
-        name:'dsLogin',
-        data () {
-            return {
-            
-            }
-        },
-        methods: {
-            addNewTodo: function () {
-            
-            }
-        }
+export default {
+  props: ["type"],
+  name: "dsLogin",
+  data() {
+    return {
+      
     }
+  },
+  mounted(){
+      var r = document.getElementById("box");
+      if(this.type == 1){
+          r.style = "left:50%";
+      }else if(this.type == 2){
+        r.style = 'right:0';
+      };
+  },
+  methods: {
+    addNewTodo: function() {}
+  }
+};
 </script>
 
 <style>
-    #login{
-        position: relative;
-    }
-    .login-box{
-        width: 500px;
-        height: 300px;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-top: 100px;
-        margin-left: -250px;
-    }
-    .button{
-        width: 442px;
-    }
-    .login-box-right{
-        position: absolute;
-        right:0;
-    }
-    .bottom-text{
-        position: absolute;
-        bottom:-560px;
-        left: 35%;
-    }
+#login {
+  position: relative;
+}
+.login-box {
+  width: 500px;
+  height: 300px;
+  position: absolute;
+  top: 50%;
+  /* left: 50%; */
+  margin-top: 100px;
+  margin-left: -250px;
+}
+.button {
+  width: 442px;
+}
+.login-box-right {
+  position: absolute;
+  right: 0;
+}
+.bottom-text {
+  position: absolute;
+  bottom: -560px;
+  left: 35%;
+}
 </style>
 
